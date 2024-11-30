da6aa1c5c92404d3e2a118555c2c99b2b6fb444d
Sharafathbrucekitt <120270427+Sharafathbrucekitt@users.noreply.github.com>
2024-11-30 20:19:58 +0000

    db conf file created

---
+++ .github/public/asp57_11_20/dbconf.php
@@ -0,0 +1,20 @@
+<?php
+define('SERVERNAME','127.0.0.1:3306');
+define('USERNAME','root');
+define('PASSWORD','mariadb');
+define('DNAME','school');
+
+try{
+
+$connect=mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DNAME);
+    if(!$connect){
+        die("connection failed".mysqli_connect_error());
+    }
+    else{
+        echo "connectection successfully";
+    }
+}
+catch (Exception $e){
+    die($e->getMessage());
+}
+?>
